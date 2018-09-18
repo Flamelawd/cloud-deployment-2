360404c4b8b60658b46eb6ec0824b1113aac9696
Gavin <36795443+Flamelawd@users.noreply.github.com>
2018-09-17 20:51:03 -0400

    Cluster build 1
    changed code to match assignment specifications, first build--hope this works!!

--- profile.py
+++ profile.py
@@ -16,12 +16,37 @@ pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
- 
-# Add a raw PC to the request.
-node = request.RawPC("node")
+ # Instantiate the amount of nodes in the cluster
+num_nodes = 4
 
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+# Create a list to hold the nodes to be placed in the cluster
+list_nodes = list()
 
-# Print the RSpec to the enclosing page.
+# Create the links for clusters to be used later
+link = request.LAN("lan")
+
+# Create all 4 nodes and their individual names
+for x in range(num_nodes):
+    list_nodes.append(request.XenVM("node-"+str(x+1)))
+  
+y=1
+for node in list_nodes:
+     node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+     node.addService(pg.Execute(shell="sh", command="sudo local/repository/silly.sh"))
+   
+     # Conditional to allow only node-1 to be able to connect to the internet
+     if y == 1:
+         node.routable_control_ip = "true"
+         y == 0
+        
+# Create empty list for interfaces
+interfaces = list()
+ifaces_len = len(list_nodes)
+for z in range(0, ifaces_len):
+    interfaces.append(list_nodes[z].addInterface("if1"))
+    interfaces[z].component_id = "eth1"
+    interfaces[z].addAddress(pg.IPv4Address("192.168.1."+str(z+1),"255.255.255.0"))
+    link.addInterface(interfaces[z])
+                                                 
 pc.printRequestRSpec(request)
+                            
